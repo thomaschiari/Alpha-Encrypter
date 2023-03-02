8da9b6d4e68e466752a2cb4f72ba8d5efa655310
thomaschiari <thomaschiari@gmail.com>
2023-03-02 16:57:05 -0300

    para_one_hot and para_string done

---
+++ encoder.py
@@ -0,0 +1,33 @@
+import numpy as np
+import unidecode
+import string
+
+def para_one_hot(msg):
+
+    M = np.zeros((37, len(msg)))
+    msg = msg.lower()
+    msg = unidecode.unidecode(msg)
+    char_list = list(string.ascii_lowercase)
+    numbers = list(range(10))
+    char_list.extend(numbers)
+    char_list.append(' ')
+
+    dict_char = {char_list[i]: i for i in range(len(char_list))}
+
+    for i in range(len(msg)):
+        M[dict_char[msg[i]], i] = 1
+
+    return M
+
+def para_string(M: np.array):
+    char_list = list(string.ascii_lowercase)
+    numbers = list(range(10))
+    char_list.extend(numbers)
+    char_list.append(' ')
+    dict_char = {i: char_list[i] for i in range(len(char_list))}
+
+    msg = ''
+    for i in range(M.shape[1]):
+        msg += dict_char[np.argmax(M[:, i])]
+
+    return msg
