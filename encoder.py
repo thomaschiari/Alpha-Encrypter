e1b2b96a0742648c14d9ce19db4960b596a059e2
thomaschiari <thomaschiari@gmail.com>
2023-03-10 16:31:29 -0300

    encoder.py updated with classes

--- encoder.py
+++ encoder.py
@@ -3,33 +3,26 @@ import unidecode
 import string
 
 
-def para_one_hot(msg):
-    M = np.zeros((37, len(msg)))
-    msg = msg.lower()
-    msg = unidecode.unidecode(msg)
-    char_list = list(string.ascii_lowercase)
-    numbers = list(range(10))
-    char_list.extend(numbers)
-    char_list.append(' ')
+class EnigmaEncoder:
 
-    dict_char = {str(char_list[i]): i for i in range(len(char_list))}
-    print(dict_char)
+    def __init__(self):
+        char_list = list(string.ascii_lowercase)
+        numbers = list(range(10))
+        char_list.extend(numbers)
+        char_list.append(' ')
+        self.dict_char = {char_list[i]: i for i in range(len(char_list))}
 
-    for i in range(len(msg)):
-        M[dict_char[msg[i]], i] = 1
+    def para_one_hot(self, msg):
+        M = np.zeros((37, len(msg)))
+        msg = msg.lower()
+        msg = unidecode.unidecode(msg)
+        for i in range(len(msg)):
+            M[self.dict_char[msg[i]], i] = 1
+        return M
 
-    return M
+    def para_string(self, M: np.array):
+        msg = ''
+        for i in range(M.shape[1]):
+            msg += self.dict_char[np.argmax(M[:, i])]
+        return msg
 
-
-def para_string(M: np.array):
-    char_list = list(string.ascii_lowercase)
-    numbers = list(range(10))
-    char_list.extend(numbers)
-    char_list.append(' ')
-    dict_char = {i: str(char_list[i]) for i in range(len(char_list))}
-
-    msg = ''
-    for i in range(M.shape[1]):
-        msg += dict_char[np.argmax(M[:, i])]
-
-    return msg
