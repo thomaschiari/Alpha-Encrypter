5ad407d59aa46ebf57c29cd77d7fe020a7a33c1b
Maraba23 <78489243+Maraba23@users.noreply.github.com>
2023-03-12 14:39:09 -0300

    a ida ta indo a volta nao

--- enigma.feature
+++
@@ -1,8 +0,0 @@
-# Created by thomaschiari at 12/03/2023
-Feature: Login
-Scenario: Successful Login
-Given I am on the login page
-When I enter my valid username and password
-And I click on the login button
-Then I should be redirected to the home page
-And I should see a welcome message
--- test_api.py
+++ test_api.py
@@ -6,13 +6,13 @@ import numpy as np
 url_enigma = 'http://localhost:5000/api/enigma'
 url_de_enigma = 'http://localhost:5000/api/de_enigma'
 
-msg = 'hello world'
-P = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-E = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+P = np.random.permutation(np.eye(37))
+P = P.tolist()
+E = np.random.permutation(np.eye(37))
+E = E.tolist()
+
+msg = "hello world"
 
 response_enigma = requests.post(url_enigma, json={'msg': msg, 'P': P, 'E': E})
-response_de_enigma = requests.post(url_de_enigma, json={'msg': response_enigma.text, 'P': P, 'E': E})
+print(response_enigma.json())
 
-print('Original message:', msg)
-print('Encoded message:', response_enigma.text)
-print('Decoded message:', response_de_enigma.text)
