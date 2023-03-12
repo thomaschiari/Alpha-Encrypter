64bdd652dbb13c672f102108e9af529ecae7ec21
Maraba23 <78489243+Maraba23@users.noreply.github.com>
2023-03-12 15:35:41 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='AlphaEncrypter',
     packages=find_packages(include=['AlphaEncrypter']),
-    version='0.1.0',
+    version='0.1.1',
     description='Dont be Sigma, be Alpha, encrypt your messages with AlphaEncrypter',
     author='Marcelo Rabello Barranco, Thomas Chiari Ciocchetti de Souza',
     license='MIT',
