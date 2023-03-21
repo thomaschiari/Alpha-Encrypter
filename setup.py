236a352e113de501d819231d0c8657c06c5dc140
thomaschiari <thomaschiari@gmail.com>
2023-03-21 07:45:41 -0300

    setup.py updated

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='AlphaEncrypter',
     packages=find_packages(include=['AlphaEncrypter']),
-    version='0.1.1',
+    version='0.1.2',
     description='Dont be Sigma, be Alpha, encrypt your messages with AlphaEncrypter',
     author='Marcelo Rabello Barranco, Thomas Chiari Ciocchetti de Souza',
     license='MIT',
